5a22522f1e4298f7a31c2db6fa44f17699237cc2
com.xin <kleine4augen@gmail.com>
2023-05-04 00:12:36 +0200

    use Random class

--- book-backend/src/main/java/com/xin/bookbackend/model/book/GoogleBook.java
+++ book-backend/src/main/java/com/xin/bookbackend/model/book/GoogleBook.java
@@ -1,21 +1,17 @@
 package com.xin.bookbackend.model.book;
 
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.List;
-import java.util.Random;
 
 public record GoogleBook(String id, VolumeInfo volumeInfo) {
 
-    public Book toBook() throws NoSuchAlgorithmException {
+    public Book toBook() {
         String id = this.id();
         String tittle = volumeInfo.title();
         List<String> authors = volumeInfo.authors();
         String publisher = volumeInfo.publisher();
         String publishedDate = volumeInfo.publishedDate();
         String description = volumeInfo.description();
-        Random r= SecureRandom.getInstanceStrong();
-        Double averageRating = volumeInfo.averageRating() == null ? (r.nextDouble() * 5)
+        Double averageRating = volumeInfo.averageRating() == null ? (Math.random() * 5)
                 : volumeInfo.averageRating();
         String imageUrl = volumeInfo.imageLinks() != null ? volumeInfo.imageLinks().thumbnail() : null;
         return new Book(id, tittle, authors, publisher, publishedDate, description, averageRating, imageUrl);
