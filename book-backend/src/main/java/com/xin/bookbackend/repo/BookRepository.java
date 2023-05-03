d75bd8ae17d8648dff45cbbd7b1c5bf0f26814d3
com.xin <kleine4augen@gmail.com>
2023-05-03 14:47:25 +0200

    fix add same book issue

--- book-backend/src/main/java/com/xin/bookbackend/repo/BookRepository.java
+++ book-backend/src/main/java/com/xin/bookbackend/repo/BookRepository.java
@@ -5,13 +5,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface BookRepository extends MongoRepository<Book, String> {
     List<Book> findByUserId(String userId);
-
-   Optional<Book> findFirstByGoogleBookId(String googleBookId);
-
     void deleteByIdAndUserId(String id, String userId);
 }
