fb17ec259ea98bd66bd4048bc827907c68ef819b
com.xin <kleine4augen@gmail.com>
2023-05-04 23:34:16 +0200

    convert book to bookDTO

--- book-backend/src/main/java/com/xin/bookbackend/model/book/BookDTO.java
+++ book-backend/src/main/java/com/xin/bookbackend/model/book/BookDTO.java
@@ -10,21 +10,6 @@ public record BookDTO(String id, String googleBookId, String title, List<String>
         this(null, googleBookId, title, authors, publisher, publishedDate, description, averageRating, imageUrl, null);
     }
 
-    public BookDTO(String googleBookId, String title, List<String> authors, String publisher,
-                   String publishedDate, Double averageRating, String userId) {
-        this(null, googleBookId, title, authors, publisher, publishedDate, null, averageRating, null, userId);
-    }
-
-
-    public BookDTO(String id, String googleBookId, String title, List<String> authors, String publisher,
-                   String publishedDate, Double averageRating, String userId) {
-        this(id, googleBookId, title, authors, publisher, publishedDate, null, averageRating, null, userId);
-    }
-
-    public BookDTO() {
-        this(null,null,null,null,null,null,null,null,null,null);
-    }
-
     public BookDTO withUserId(String userId) {
         return new BookDTO(null, googleBookId, title, authors, publisher, publishedDate, description, averageRating, imageUrl, userId);
     }
