0c85d631847cd2b22b4f99922819a89b018e0ced
com.xin <kleine4augen@gmail.com>
2023-05-05 11:57:10 +0200

    fix convert bookDTO issue

--- book-frontend/src/componet/ProtectedRoutes.tsx
+++ book-frontend/src/componet/ProtectedRoutes.tsx
@@ -5,7 +5,7 @@ type Props = {
     username: string|undefined
 }
 export default function ProtectedRoutes(props: Props) {
-    const isAuthenticated = props.username != undefined && props.username != 'anonymousUser'
+    const isAuthenticated = props.username !== undefined && props.username !== 'anonymousUser'
     return (
         isAuthenticated ? <Outlet/> : <Navigate to={"/login"}/>
     )
