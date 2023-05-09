38343d016fd9a8a78af4f242611f883354f4c758
com.xin <kleine4augen@gmail.com>
2023-05-09 16:10:34 +0200

    add 2 tests

--- book-backend/src/test/java/com/xin/bookbackend/service/BookServiceTest.java
+++ book-backend/src/test/java/com/xin/bookbackend/service/BookServiceTest.java
@@ -11,8 +11,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
@@ -62,7 +62,7 @@ class BookServiceTest {
     }
 
     @Test
-    void addBook() {
+    void addBook_Successfully() {
         String userId = UUID.randomUUID().toString();
         Book book = new Book("5eDWcLzdAcYC",
                 "Java von Kopf bis Fuß",
@@ -76,7 +76,24 @@ class BookServiceTest {
     }
 
     @Test
-    void updateBookById() {
+    void addBook_failed() {
+        String userId = UUID.randomUUID().toString();
+        Book book = new Book("5eDWcLzdAcYC",
+                "Java von Kopf bis Fuß",
+                List.of("Kathy Sierra", "Bert Bates"),
+                "O'Reilly Germany",
+                "2006", 4.5, userId);
+
+        when(bookRepo.findByUserId(userId)).thenReturn(List.of(book));
+
+
+        assertThrows(IllegalArgumentException.class, () ->
+                service.addBook(book, userId));
+    }
+
+
+    @Test
+    void updateBookById_Successfully() {
         String id = UUID.randomUUID().toString();
 
         String userId = UUID.randomUUID().toString();
@@ -102,6 +119,25 @@ class BookServiceTest {
         updatedBook = updatedBook.withId(id);
         verify(bookRepo).save(updatedBook);
     }
+
+    @Test
+    void updateBookById_failed() {
+        String id = UUID.randomUUID().toString();
+
+        String userId = UUID.randomUUID().toString();
+        Book book = new Book("5eDWcLzdAcYC",
+                "Java von Kopf bis Fuß",
+                List.of("Kathy Sierra", "Bert Bates"),
+                "O'Reilly Germany",
+                "2006", 4.5, userId);
+
+
+        when(bookRepo.findById(id)).thenReturn(Optional.empty());
+
+
+        assertThrows(NoSuchElementException.class, () -> service.updateBookById(id, book));
+    }
+
 
     @Test
     void deleteBookByIdAndUserId() {
