b1107a66adffc36bc4da58f6daa301bef7342e12
com.xin <kleine4augen@gmail.com>
2023-05-10 09:55:38 +0200

    objectMapper

--- book-backend/src/test/java/com/xin/bookbackend/controller/UserControllerTest.java
+++ book-backend/src/test/java/com/xin/bookbackend/controller/UserControllerTest.java
@@ -7,7 +7,6 @@ import com.xin.bookbackend.model.user.MongoUserDTO;
 import com.xin.bookbackend.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
