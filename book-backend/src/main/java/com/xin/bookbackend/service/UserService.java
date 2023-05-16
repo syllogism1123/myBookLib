25884ef6ae4ff9bb6cda6573f264759605471319
com.xin <kleine4augen@gmail.com>
2023-05-16 19:52:34 +0200

    fix signup issue

--- book-backend/src/main/java/com/xin/bookbackend/service/UserService.java
+++ book-backend/src/main/java/com/xin/bookbackend/service/UserService.java
@@ -23,9 +23,11 @@ public class UserService {
 
         if (mongoUserRepository.findMongoUserByUsername(mongoUserDTO.username()).isPresent()) {
             throw new IllegalArgumentException("The username already exists.");
-        } else if (mongoUserRepository.findMongoUserByEmail(mongoUserDTO.email()).isPresent()) {
-            throw new IllegalArgumentException("The email address already exists.");
         } else {
+            if (mongoUserRepository.findMongoUserByEmail(mongoUserDTO.email()).isPresent()) {
+                throw new IllegalArgumentException("The email address already exists.");
+            }
+
             String encodedPassword = encoder.encode(mongoUserDTO.password());
             MongoUser encodedUser = new MongoUser(mongoUserDTO.username(), encodedPassword, mongoUserDTO.firstname(), mongoUserDTO.lastname(), mongoUserDTO.email());
             return mongoUserRepository.save(encodedUser);
